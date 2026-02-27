6d8c570ca98ac0cc9ea540eb6db5d7953e179fcc
BHANUPRASAD77 <131442151+BHANUPRASAD77@users.noreply.github.com>
2026-02-27 20:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let student = {
 	name : "surya" ,
 	age : 33,
-	
-		getkeys : function (obj){
-			retun Object.keys(obj)
-		}
 }
-	console.log(getKeys(student));
+function getkeys(obj){
+	retun Object.keys(obj);
+}
+console.log(getKeys(student));
 	// console.log(getkeys(student))
